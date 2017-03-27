b572f254935707217243ae016dabe0fa77f3d2bd
maarten <maarten.moens@ugent.be>
2017-03-27 14:14:27 +0000

    Added command line

--- src/powderpic.cr
+++ src/powderpic.cr
@@ -1,22 +1,31 @@
 require "./powderpic/*"
+require "option_parser"
 
 module Powderpic
-
-  ps=PictureSampler.new("test.png",0,0)
-  ps.sampler
-  string=ps.toString
-  STDOUT.print string
-  sleep 3
-  #picturesampling
-  # heightoutput=`tput lines`
-  # widthoutput=`tput cols`
-  # heightstring=heightoutput[/^[0-9]*$/]
-  # widthstring=widthoutput[/^[0-9]*$/]
-  # height=heightstring.to_i
-  # width=widthstring.to_i
-  # filling ="#{"0" * width}\n"
-  # STDOUT.print "#{filling*height}"
-  # filling ="#{"0" * width}\n"
-  # sleep 3
-  # STDOUT.print "#{filling*height}"
+  time=3.0
+  timetemp=0.0
+  filename=""
+  run=false
+  OptionParser.parse! do |parser|
+    parser.banner = "Usage: powderpic [arguments]"
+    parser.on("-f NAME", "Specifies the filename. Program will not run without this argument"){ |name| filename = name }
+    parser.on("-t TIMER", "Specifies the time the terminal sleeps on the picture. If not given declared, sleeps for 3 seconds") { |timer| timetemp = timer.to_f }
+    parser.on("-h", "--help", "Show this help") { puts parser }
+  end
+  if timetemp>0.0
+    time=timetemp
+  end
+  if filename!=""
+    heightoutput=`tput lines`
+    widthoutput=`tput cols`
+    heightstring=heightoutput[/^[0-9]*$/]
+    widthstring=widthoutput[/^[0-9]*$/]
+    height=heightstring.to_i
+    width=widthstring.to_i
+    ps=PictureSampler.new(filename,width,height)
+    ps.sample
+    string=ps.toString
+    STDOUT.print string
+    sleep time
+  end
 end
--- src/powderpic/picturesampler.cr
+++ src/powderpic/picturesampler.cr
@@ -1,23 +1,51 @@
 require "stumpy_png"
 class PictureSampler
-  property canvas : StumpyCore::Canvas,str : String, sampled : Array(Array(Int32)), width , height
+  property canvas : StumpyCore::Canvas,str : String, sampled : Array(Array(Int32)), width : Int32 , height : Int32
   def initialize(filename : String ,width : Int32,height : Int32)
-    @width=width
     @canvas=StumpyPNG.read(filename)
-    @height=height
-    @sampled=Array(Array(Int32)).new(@canvas.width) { Array(Int32).new(@canvas.height,0) }
+    puts "width ...."
+    @width=closestDivisor(@canvas.width, width)
+    puts "height ...."
+    @height=closestDivisor(@canvas.height, height)
+    @sampled=Array(Array(Int32)).new(@width) { Array(Int32).new(@height,0) }
     @str=""
     puts "initialized"
   end
-  def sampler
-    (0...@canvas.width).each do |x|
-      (0...@canvas.height).each do |y|
-        r, g, b = @canvas[x, y].to_rgb8
+  def sample
+    columnsamples=@canvas.height/@height
+    rowsamples=@canvas.width/@width
+    puts "#{columnsamples} #{@canvas.height} #{@height}"
+    puts "#{rowsamples} #{@canvas.width} #{@width}"
+    (0...@width).each do |x|
+      (0...@height).each do |y|
+        r=0
+        g=0
+        b=0
+        (0...rowsamples).each do |s|
+          (0...columnsamples).each do |t|
+            if(((x*rowsamples)+s)>@canvas.width||((y*columnsamples)+t)>@canvas.height)
+              puts "ERROR"
+              puts "x*rowsamples+s #{x*rowsamples+s} width #{@canvas.width} x #{x} rowsamples #{rowsamples} s #{s}"
+              puts "y*columnsamples+t #{y*columnsamples+t} height #{@canvas.height}"
+            end
+            r0, g0, b0 = @canvas[(x*rowsamples)+s, (y*columnsamples)+t].to_rgb8
+            r+=r0
+            g+=g0
+            b+=b0
+          end
+        end
+        r/=columnsamples*rowsamples
+        g/=columnsamples*rowsamples
+        b/=columnsamples*rowsamples
         @sampled[x][y]=rgbtoansi(r,g,b)
       end
     end
   end
   def toString
+    puts "Sampled[0].size"
+    puts @sampled[0].size
+    puts "Sampled.size"
+    puts @sampled.size
     (0...@sampled[0].size).each do |y|
       (0...@sampled.size).each do |x|
         @str=@str+colorize(@sampled[x][y]," ")
@@ -35,7 +63,14 @@ class PictureSampler
     sample+=(b/43)
     return sample
   end
-
+  private def closestDivisor(big, small)
+    puts small
+    while big % small != 0
+      small-=1
+      puts small
+    end
+    return small
+  end
   private def colorize(colornumber,text)
     #inspired by http://stackoverflow.com/questions/1489183/colorized-ruby-output
     "\e[48;5;#{colornumber}m#{text}\e[0m"
